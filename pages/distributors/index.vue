e2ada0eeff65618277e5a3e20b093be338869219
Tariq Ali <tariqali@Tariqs-MacBook-Pro.local>
2021-11-29 14:09:06 +0300

    update

--- pages/distributors/index.vue
+++ pages/distributors/index.vue
@@ -1,8 +1,18 @@
 <template>
-  <div id="LibraryPage">
+  <div id="distributors-page">
+    <v-dialog v-model="createDistributorDialog" max-width="750px" transition="slide-y-transition">
+      <v-card color="secondary" class="shadow-1 radius-1 pa-10">
+        <v-form ref="createDistributorRef" v-model="createDistributor" lazy-validation>
+          <v-row>
+            <v-col cols="12" sm="12" md="6" lg="6" xl="6"></v-col>
+          </v-row>
+        </v-form>
+      </v-card>
+    </v-dialog>
+
     <v-data-table
       :headers="headers"
-      :items.sync="attachments"
+      :items.sync="distributors"
       :items-per-page="15"
       item-key="courseId"
       class="shadow-1 radius-1 pa-5 secondary"
@@ -34,13 +44,38 @@
                   v-on="on"
                   @click="$router.push('/courses/add')"
                 >
-                  <v-icon>mdi-upload</v-icon>
+                  <v-icon>add</v-icon>
                 </v-btn>
               </template>
-              <span class="primary--text">رفع ملف</span>
+              <span class="primary--text">اضافة موزع جديد</span>
             </v-tooltip>
           </div>
         </v-toolbar>
+      </template>
+
+      <template #[`item.location`]="{ item }">
+        <v-btn
+          color="error"
+          icon
+          :href="`https://www.google.com/maps/@${item.lang},${item.lat}`"
+          target="_blank"
+        >
+          <v-icon>location_on</v-icon>
+        </v-btn>
+      </template>
+
+      <template #[`item.distributorPhoto`]="{ item }">
+        <v-img
+          :src="$axios.defaults.baseURL + item.distributorPhoto"
+          max-width="50px"
+          height="50px"
+        ></v-img>
+      </template>
+
+      <template #[`item.actions`]="{ item }">
+        <v-btn color="error" icon @click="deleteDistributor(item)">
+          <v-icon>delete</v-icon>
+        </v-btn>
       </template>
     </v-data-table>
   </div>
@@ -54,17 +89,70 @@ export default {
       {
         text: 'المعرف',
         align: 'start',
-        value: 'attachemntId',
+        value: 'idDistributor',
         sortable: false,
       },
-      { text: 'اسم الملف', value: 'attachmentName', sortable: false },
-      { text: 'المادة', value: 'attachmentSubject', sortable: false },
-      { text: 'التاريخ', value: 'createdAt', sortable: false },
+      { text: 'الصورة', value: 'distributorPhoto', sortable: false },
+      { text: 'اسم المستخدم', value: 'userName', sortable: false },
+      { text: 'عن الموزع', value: 'distributorBio', sortable: false },
+      { text: 'اسم المكتبة', value: 'libraryName', sortable: false },
+      { text: 'الهاتف', value: 'phone', sortable: false },
+      { text: 'المحافظة', value: 'province.provinceName', sortable: false },
+      { text: 'الخريطة', value: 'location', sortable: false },
       { text: 'الاجرائات', value: 'actions', sortable: false },
     ],
 
-    attachments: [],
+    distributors: [],
+    provinces: [],
+    roles: [],
+    provinceId: null,
+    roleId: null,
+    username: null,
+    password: null,
+    phone: null,
+    distributorPhoto: null,
+    distributorBio: null,
+    libraryName: null,
   }),
+
+  mounted() {
+    this.getDistributors()
+  },
+
+  methods: {
+    async getDistributors() {
+      try {
+        const distributors = await this.$axios.get(`distributorInfos`)
+        const users = []
+        for (let i = 0; i < distributors.data.length; i++) {
+          const user = await this.$axios.get(
+            `user/${distributors.data[i].userId}`
+          )
+          users.push({
+            ...user.data,
+            ...distributors.data[i],
+          })
+        }
+
+        this.distributors = users
+      } catch (e) {
+        console.error(e.response)
+      }
+    },
+
+    async deleteDistributor(item) {
+      if (confirm('هل تريد حذف الموزع ؟')) {
+        try {
+          const deleteDist = await this.$axios.delete(
+            `distributorInfo/${item.idDistributor}`
+          )
+          console.log(deleteDist)
+        } catch (error) {
+          console.log(error.response)
+        }
+      }
+    },
+  },
 }
 </script>
 
