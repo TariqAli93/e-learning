dae7131872db091805b2d0c1e24b75d5b24463d2
Tariq Ali <tariqali@Tariqs-MacBook-Pro.local>
2021-11-30 16:34:48 +0300

    update

--- pages/distributors/index.vue
+++ pages/distributors/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="distributors-page">
-    <v-dialog v-model="createDistributorDialog" max-width="750px" transition="slide-y-transition">
+    <v-dialog
+      v-model="createDistributorDialog"
+      max-width="750px"
+      transition="slide-y-transition"
+    >
       <v-card color="secondary" class="shadow-1 radius-1 pa-10">
         <v-toolbar color="primary" class="shadow-1 radius-1 mb-10">
           <h4>انشاء موزع جديد</h4>
@@ -9,46 +13,128 @@
             <v-icon>close</v-icon>
           </v-btn>
         </v-toolbar>
-        <v-form ref="createDistributorRef" v-model="createDistributorModel" lazy-validation @submit.prevent="createDistributor">
+        <v-form
+          ref="createDistributorRef"
+          v-model="createDistributorModel"
+          lazy-validation
+          @submit.prevent="createDistributor"
+        >
           <v-row>
             <v-col cols="12" sm="12" md="4" lg="4" xl="4">
-              <v-text-field v-model="username" prepend-inner-icon="person" color="text" outlined label="اسم المستخدم" :rules="rules"></v-text-field>
+              <v-text-field
+                v-model="username"
+                prepend-inner-icon="person"
+                color="text"
+                outlined
+                label="اسم المستخدم"
+                :rules="rules"
+              ></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="12" md="4" lg="4" xl="4">
-              <v-text-field v-model="password" :type="showPassword ? 'text' : 'password'" :append-icon="showPassword ? 'visibility_off' : 'visibility'" @click:append="showPassword = !showPassword" prepend-inner-icon="password" color="text" outlined label="كلمة المرور" :rules="rules"></v-text-field>
+              <v-text-field
+                v-model="password"
+                :type="showPassword ? 'text' : 'password'"
+                :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+                prepend-inner-icon="password"
+                color="text"
+                outlined
+                label="كلمة المرور"
+                :rules="rules"
+                @click:append="showPassword = !showPassword"
+              ></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="12" md="4" lg="4" xl="4">
-              <v-text-field v-model="phone" prepend-inner-icon="drag_indicator" color="text" outlined label="رقم الهاتف" :rules="rules"></v-text-field>
+              <v-text-field
+                v-model="phone"
+                prepend-inner-icon="drag_indicator"
+                color="text"
+                outlined
+                label="رقم الهاتف"
+                :rules="rules"
+              ></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="12" md="4" lg="4" xl="4">
-              <v-text-field v-model="libraryName" prepend-inner-icon="local_library" color="text" outlined label="اسم المكتبة" :rules="rules"></v-text-field>
+              <v-text-field
+                v-model="libraryName"
+                prepend-inner-icon="local_library"
+                color="text"
+                outlined
+                label="اسم المكتبة"
+                :rules="rules"
+              ></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="12" md="4" lg="4" xl="4">
-              <v-text-field v-model="longitude" prepend-inner-icon="person_pin" color="text" outlined label="خط الطول (longitude)" :rules="rules"></v-text-field>
+              <v-text-field
+                v-model="longitude"
+                prepend-inner-icon="person_pin"
+                color="text"
+                outlined
+                label="خط الطول (longitude)"
+                :rules="rules"
+              ></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="12" md="4" lg="4" xl="4">
-              <v-text-field v-model="latitude" prepend-inner-icon="person_pin" color="text" outlined label="خط العرض (latitude)" :rules="rules"></v-text-field>
+              <v-text-field
+                v-model="latitude"
+                prepend-inner-icon="person_pin"
+                color="text"
+                outlined
+                label="خط العرض (latitude)"
+                :rules="rules"
+              ></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="12" md="6" lg="6" xl="6">
-              <v-text-field v-model="provinceId" prepend-inner-icon="map" color="text" outlined label="المحافظة" :rules="rules"></v-text-field>
+              <v-select
+                v-model="provinceId"
+                prepend-inner-icon="map"
+                color="text"
+                outlined
+                label="المحافظة"
+                :rules="rules"
+                :items="provinces"
+                item-color="text"
+                item-value="idProvince"
+                item-text="provinceName"
+              ></v-select>
             </v-col>
 
             <v-col cols="12" md="6" lg="6" xl="6">
-              <v-text-field v-model="distributorPhoto" prepend-inner-icon="collections" color="text" outlined label="الصورة" :rules="rules"></v-text-field>
+              <v-file-input
+                prepend-icon="collections"
+                color="text"
+                outlined
+                label="الصورة"
+                :rules="rules"
+                @change="saveImage"
+              ></v-file-input>
             </v-col>
 
             <v-col cols="12">
-              <v-textarea v-model="distributorBio" prepend-inner-icon="format_size" color="text" outlined label="عن الموزع" :rules="rules"></v-textarea>
+              <v-textarea
+                v-model="distributorBio"
+                prepend-inner-icon="format_size"
+                color="text"
+                outlined
+                label="عن الموزع"
+                :rules="rules"
+              ></v-textarea>
             </v-col>
           </v-row>
 
-          <v-btn color="success" large block depressed :disabled="!createDistributorModel" type="submit">
+          <v-btn
+            color="success"
+            large
+            block
+            depressed
+            :disabled="!createDistributorModel"
+            type="submit"
+          >
             حفظ المعلومات
           </v-btn>
         </v-form>
@@ -158,6 +244,7 @@ export default {
     password: null,
     phone: null,
     distributorPhoto: null,
+    uploadedImage: null,
     distributorBio: null,
     libraryName: null,
     showPassword: false,
@@ -168,12 +255,51 @@ export default {
 
   mounted() {
     this.getDistributors()
+    this.$axios.get('provinces').then((result) => {
+      this.provinces = result.data
+    })
   },
 
   methods: {
+    saveImage(event) {
+      this.uploadedImage = event !== null ? event : null
+    },
+
     async createDistributor() {
       // create user first
-      // then create distributorsInfo
+      const uploadedImage = new FormData()
+      uploadedImage.append('attachment', this.uploadedImage)
+
+      try {
+        const upload = await this.$axios.post('upload', uploadedImage)
+
+        this.distributorPhoto = upload.data.imagePath
+
+        const createUsers = await this.$axios.post('addUser', {
+          userName: this.username,
+          password: this.password,
+          phone: this.phone,
+          roleId: 4,
+          provinceId: this.provinceId * 1,
+        })
+
+        const createDistributors = await this.$axios.post(
+          'addDistributorInfo',
+          {
+            lang: this.longitude,
+            lat: this.latitude,
+            libraryName: this.libraryName,
+            distributorPhoto: this.distributorPhoto,
+            distributorBio: this.distributorBio,
+            userId: createUsers.data.idUser
+          }
+        )
+
+        this.getDistributors()
+        this.createDistributorDialog = false
+      } catch (error) {
+        console.log(error)
+      }
     },
 
     async getDistributors() {
@@ -191,7 +317,6 @@ export default {
         }
 
         this.distributors = users
-        console.log(this.distributors)
       } catch (e) {
         console.error(e.response)
       }
@@ -203,7 +328,7 @@ export default {
           const deleteDist = await this.$axios.delete(
             `distributorInfo/${item.idDistributor}`
           )
-          console.log(deleteDist)
+          this.getDistributors()
         } catch (error) {
           console.log(error.response)
         }
--- pages/teachers/index.vue
+++ pages/teachers/index.vue
@@ -1,8 +1,149 @@
 <template>
-  <div id="LibraryPage">
+  <div id="distributors-page">
+    <v-dialog
+      v-model="createDistributorDialog"
+      max-width="750px"
+      transition="slide-y-transition"
+    >
+      <v-card color="secondary" class="shadow-1 radius-1 pa-10">
+        <v-toolbar color="primary" class="shadow-1 radius-1 mb-10">
+          <h4>انشاء استاذ جديد</h4>
+          <v-spacer />
+          <v-btn color="error" icon @click="createDistributorDialog = false">
+            <v-icon>close</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-form
+          ref="createDistributorRef"
+          v-model="createDistributorModel"
+          lazy-validation
+          @submit.prevent="createDistributor"
+        >
+          <v-row>
+            <v-col cols="12" sm="12" md="4" lg="4" xl="4">
+              <v-text-field
+                v-model="username"
+                prepend-inner-icon="person"
+                color="text"
+                outlined
+                label="اسم المستخدم"
+                :rules="rules"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="12" md="4" lg="4" xl="4">
+              <v-text-field
+                v-model="password"
+                :type="showPassword ? 'text' : 'password'"
+                :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+                prepend-inner-icon="password"
+                color="text"
+                outlined
+                label="كلمة المرور"
+                :rules="rules"
+                @click:append="showPassword = !showPassword"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="12" md="4" lg="4" xl="4">
+              <v-text-field
+                v-model="phone"
+                prepend-inner-icon="drag_indicator"
+                color="text"
+                outlined
+                label="رقم الهاتف"
+                :rules="rules"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="12" md="4" lg="4" xl="4">
+              <v-text-field
+                v-model="libraryName"
+                prepend-inner-icon="local_library"
+                color="text"
+                outlined
+                label="اسم المكتبة"
+                :rules="rules"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="12" md="4" lg="4" xl="4">
+              <v-text-field
+                v-model="longitude"
+                prepend-inner-icon="person_pin"
+                color="text"
+                outlined
+                label="خط الطول (longitude)"
+                :rules="rules"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="12" md="4" lg="4" xl="4">
+              <v-text-field
+                v-model="latitude"
+                prepend-inner-icon="person_pin"
+                color="text"
+                outlined
+                label="خط العرض (latitude)"
+                :rules="rules"
+              ></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="12" md="6" lg="6" xl="6">
+              <v-select
+                v-model="provinceId"
+                prepend-inner-icon="map"
+                color="text"
+                outlined
+                label="المحافظة"
+                :rules="rules"
+                :items="provinces"
+                item-color="text"
+                item-value="idProvince"
+                item-text="provinceName"
+              ></v-select>
+            </v-col>
+
+            <v-col cols="12" md="6" lg="6" xl="6">
+              <v-file-input
+                prepend-icon="collections"
+                color="text"
+                outlined
+                label="الصورة"
+                :rules="rules"
+                @change="saveImage"
+              ></v-file-input>
+            </v-col>
+
+            <v-col cols="12">
+              <v-textarea
+                v-model="distributorBio"
+                prepend-inner-icon="format_size"
+                color="text"
+                outlined
+                label="عن الموزع"
+                :rules="rules"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+
+          <v-btn
+            color="success"
+            large
+            block
+            depressed
+            :disabled="!createDistributorModel"
+            type="submit"
+          >
+            حفظ المعلومات
+          </v-btn>
+        </v-form>
+      </v-card>
+    </v-dialog>
+
     <v-data-table
       :headers="headers"
-      :items.sync="attachments"
+      :items.sync="distributors"
       :items-per-page="15"
       item-key="courseId"
       class="shadow-1 radius-1 pa-5 secondary"
@@ -32,15 +173,40 @@
                   v-bind="attrs"
                   class="mr-10"
                   v-on="on"
-                  @click="$router.push('/courses/add')"
+                  @click="createDistributorDialog = true"
                 >
-                  <v-icon>mdi-upload</v-icon>
+                  <v-icon>add</v-icon>
                 </v-btn>
               </template>
-              <span class="primary--text">رفع ملف</span>
+              <span class="primary--text">اضافة استاذ جديد</span>
             </v-tooltip>
           </div>
         </v-toolbar>
+      </template>
+
+      <template #[`item.location`]="{ item }">
+        <v-btn
+          color="error"
+          icon
+          :href="`https://www.google.com/maps/@${item.lang},${item.lat}`"
+          target="_blank"
+        >
+          <v-icon>location_on</v-icon>
+        </v-btn>
+      </template>
+
+      <template #[`item.distributorPhoto`]="{ item }">
+        <v-img
+          :src="$axios.defaults.baseURL + item.distributorPhoto"
+          max-width="50px"
+          height="50px"
+        ></v-img>
+      </template>
+
+      <template #[`item.actions`]="{ item }">
+        <v-btn color="error" icon @click="deleteDistributor(item)">
+          <v-icon>delete</v-icon>
+        </v-btn>
       </template>
     </v-data-table>
   </div>
@@ -54,17 +220,121 @@ export default {
       {
         text: 'المعرف',
         align: 'start',
-        value: 'attachemntId',
+        value: 'idDistributor',
         sortable: false,
       },
-      { text: 'اسم الملف', value: 'attachmentName', sortable: false },
-      { text: 'المادة', value: 'attachmentSubject', sortable: false },
-      { text: 'التاريخ', value: 'createdAt', sortable: false },
+      { text: 'الصورة', value: 'distributorPhoto', sortable: false },
+      { text: 'اسم المستخدم', value: 'userName', sortable: false },
+      { text: 'عن الموزع', value: 'distributorBio', sortable: false },
+      { text: 'اسم المكتبة', value: 'libraryName', sortable: false },
+      { text: 'الهاتف', value: 'phone', sortable: false },
+      { text: 'المحافظة', value: 'province.provinceName', sortable: false },
+      { text: 'الخريطة', value: 'location', sortable: false },
       { text: 'الاجرائات', value: 'actions', sortable: false },
     ],
 
-    attachments: [],
+    createDistributorDialog: false,
+    createDistributorModel: false,
+    distributors: [],
+    provinces: [],
+    roles: [],
+    provinceId: null,
+    roleId: null,
+    username: null,
+    password: null,
+    phone: null,
+    distributorPhoto: null,
+    uploadedImage: null,
+    distributorBio: null,
+    libraryName: null,
+    showPassword: false,
+    latitude: null,
+    longitude: null,
+    rules: [(v) => !!v || 'لا يمكن ترك الحقل فارغ'],
   }),
+
+  mounted() {
+    this.getDistributors()
+    this.$axios.get('provinces').then((result) => {
+      this.provinces = result.data
+    })
+  },
+
+  methods: {
+    saveImage(event) {
+      this.uploadedImage = event !== null ? event : null
+    },
+
+    async createDistributor() {
+      // create user first
+      const uploadedImage = new FormData()
+      uploadedImage.append('attachment', this.uploadedImage)
+
+      try {
+        const upload = await this.$axios.post('upload', uploadedImage)
+
+        this.distributorPhoto = upload.data.imagePath
+
+        const createUsers = await this.$axios.post('addUser', {
+          userName: this.username,
+          password: this.password,
+          phone: this.phone,
+          roleId: 4,
+          provinceId: this.provinceId * 1,
+        })
+
+        const createDistributors = await this.$axios.post(
+          'addDistributorInfo',
+          {
+            lang: this.longitude,
+            lat: this.latitude,
+            libraryName: this.libraryName,
+            distributorPhoto: this.distributorPhoto,
+            distributorBio: this.distributorBio,
+            userId: createUsers.data.idUser
+          }
+        )
+
+        this.getDistributors()
+        this.createDistributorDialog = false
+      } catch (error) {
+        console.log(error)
+      }
+    },
+
+    async getDistributors() {
+      try {
+        const distributors = await this.$axios.get(`distributorInfos`)
+        const users = []
+        for (let i = 0; i < distributors.data.length; i++) {
+          const user = await this.$axios.get(
+            `user/${distributors.data[i].userId}`
+          )
+          users.push({
+            ...user.data,
+            ...distributors.data[i],
+          })
+        }
+
+        this.distributors = users
+      } catch (e) {
+        console.error(e.response)
+      }
+    },
+
+    async deleteDistributor(item) {
+      if (confirm('هل تريد حذف الموزع ؟')) {
+        try {
+          const deleteDist = await this.$axios.delete(
+            `distributorInfo/${item.idDistributor}`
+          )
+          this.getDistributors()
+        } catch (error) {
+          console.log(error.response)
+        }
+      }
+    },
+  },
 }
 </script>
 
